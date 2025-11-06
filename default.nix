f4685a145fd6f92561daa2b74b44198d200175f9
inogai <inogai.sign@gmail.com>
2025-11-06 20:07:33 +0800

    chore: delete example-package

--- default.nix
+++ default.nix
@@ -5,21 +5,15 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-
-{
-  pkgs ? import <nixpkgs> { },
-}:
-
-{
+{pkgs ? import <nixpkgs> {}}: {
   # The `lib`, `modules`, and `overlays` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
+  lib = import ./lib {inherit pkgs;}; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  fzfmenu = pkgs.callPackage ./pkgs/fzfmenu { };
-  moegi-nvim = pkgs.callPackage ./pkgs/moegi-nvim { };
-  winterm-rs = pkgs.callPackage ./pkgs/winterm-rs { };
+  fzfmenu = pkgs.callPackage ./pkgs/fzfmenu {};
+  moegi-nvim = pkgs.callPackage ./pkgs/moegi-nvim {};
+  winterm-rs = pkgs.callPackage ./pkgs/winterm-rs {};
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
